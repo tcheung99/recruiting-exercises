10639a5339243b7fae19635d5c4da022f0cea859
Trudie Cheung <trudie.cheung@gmail.com>
2020-08-29 17:58:31 -0600

    Added additional comments and renamed variables

--- inventory-allocator/src/inventory_allocation.py
+++ inventory-allocator/src/inventory_allocation.py
@@ -3,20 +3,25 @@
 # 2. Output list order is in the order of least cost (0-indexed) to highest cost (index -1)
 
 class InventoryAllocator:
-    def get_allocation(self, order, warehouse_inventories):
-        return_order = list() 
-        warehouse_order = dict()
-        for item in order.keys():
-            item_amount = order.get(item)
+    def get_allocation(self, ordered_items: dict(), warehouse_inventories: list(dict())) -> list(dict()):
+
+        # Initialize the list of warehouses to ship from (the returned list)
+        cheapest_shipment = list() 
+
+        # Initialize a dictionary to hold the necessary shipments from each warehouse 
+        warehouse_orders = dict()
+        
+        for item in ordered_items.keys():
+            item_amount = ordered_items.get(item)
 
             # Handle each item sequentially, and check each warehouse for inventory for that item
             for warehouse in (warehouse_inventories): # This is a list 
                 if (item in warehouse['inventory']) and (item_amount > 0):
                     amount_taken = min(item_amount, warehouse['inventory'][item])
-                    if not warehouse_order.get(warehouse['name'], None):
-                        warehouse_order[warehouse['name']] = {item : amount_taken}
+                    if not warehouse_orders.get(warehouse['name'], None):
+                        warehouse_orders[warehouse['name']] = {item : amount_taken}
                     else:
-                        warehouse_order[warehouse['name']].update({item : amount_taken})
+                        warehouse_orders[warehouse['name']].update({item : amount_taken})
 
                     item_amount -= warehouse['inventory'][item]
                     warehouse['inventory'][item] -= amount_taken
@@ -25,10 +30,10 @@ class InventoryAllocator:
                 # print('Insufficient inventory, order requires {} more {}(s) than available.'.format(item_amount, item))
                 return []
 
-        for (k,v) in warehouse_order.items():
-            return_order.append({k:v})
+        for (k,v) in warehouse_orders.items():
+            cheapest_shipment.append({k:v})
 
-        return return_order
+        return cheapest_shipment
 
 if __name__ == "__main__":
     NewAllocator = InventoryAllocator()
