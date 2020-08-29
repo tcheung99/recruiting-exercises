11b90a0e46a51e67999fe5dd8141c8d8b9ac37c6
Trudie Cheung <trudie.cheung@gmail.com>
2020-08-29 17:35:27 -0600

    Added comments for assumptions and removed redundant code

--- inventory-allocator/src/inventory_allocation.py
+++ inventory-allocator/src/inventory_allocation.py
@@ -1,3 +1,7 @@
+# Assumptions: 
+# 1. Warehouses are pre-sorted based on cost. 
+# 2. Output list order is in the order of least cost (0-indexed) to highest cost (index -1)
+
 class InventoryAllocator:
     def get_allocation(self, order, warehouse_inventories):
         return_order = list() 
@@ -5,17 +9,14 @@ class InventoryAllocator:
         for item in order.keys():
             item_amount = order.get(item)
 
-            # warehouse_order = dict()
             # Handle each item sequentially, and check each warehouse for inventory for that item
-            for i, warehouse in enumerate(warehouse_inventories): # This is a list 
+            for warehouse in (warehouse_inventories): # This is a list 
                 if (item in warehouse['inventory']) and (item_amount > 0):
                     amount_taken = min(item_amount, warehouse['inventory'][item])
                     if not warehouse_order.get(warehouse['name'], None):
                         warehouse_order[warehouse['name']] = {item : amount_taken}
                     else:
-                        # print(warehouse_order)
                         warehouse_order[warehouse['name']].update({item : amount_taken})
-                        # else:
 
                     item_amount -= warehouse['inventory'][item]
                     warehouse['inventory'][item] -= amount_taken
@@ -24,26 +25,16 @@ class InventoryAllocator:
                 # print('Insufficient inventory, order requires {} more {}(s) than available.'.format(item_amount, item))
                 return []
 
-        print(warehouse_order)
-        # print(warehouse_inventories)
-        # return_order.append(warehouse_order)
         for (k,v) in warehouse_order.items():
             return_order.append({k:v})
 
-        print(return_order)
         return return_order
-
-# not enough inventory in 2nd warehouse 
-# multiple items 
-# should we remove if the inventory is 0 
 
 if __name__ == "__main__":
     NewAllocator = InventoryAllocator()
 
-    # Manual testing 
+    # Manual testing
     order = { 'apple': 2, 'orange': 9 }
-    # inventories = [{ 'name': 'owd', 'inventory': { 'apple': 4 ,'orange': 6 }, 'name': 'lol', 'inventory': { 'banana': 2 ,'orange': 2 } }]
     inventories = [{ 'name': 'owd', 'inventory': { 'apple': 4 ,'orange': 6 }}, {'name': 'lol', 'inventory': { 'banana': 2 ,'orange': 2 } }]
-    print('Output: {}'.format(NewAllocator.get_allocation(order, inventories)))
 
-# Warehouses are pre-sorted based on cost. 
+    print('Output: {}'.format(NewAllocator.get_allocation(order, inventories)))
